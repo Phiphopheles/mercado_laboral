3bde9fc907a494439f4ba8235aa30225ce8f5fd8
pedro <orinocomascarpone@gmail.com>
2025-04-07 14:12:28 +0200

    Primer commit

---
+++ apps/inteligencia/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+class InteligenciaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.inteligencia'  # Ensure this matches the app's path
